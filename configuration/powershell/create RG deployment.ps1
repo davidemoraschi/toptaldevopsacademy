aa92e32b7895d71761c737ade6cffbe895ba9850
Davide Moraschi <davide.moraschi@toptal.com>
2018-12-14 17:03:06 +0100

    works with ubuntu

--- configuration/powershell/create RG deployment.ps1
+++ configuration/powershell/create RG deployment.ps1
@@ -7,4 +7,10 @@ Get-AzureRmResourceGroup -Name $ResourceGroupName | Remove-AzureRmResourceGroup 
 New-AzureRmResourceGroup -Name $ResourceGroupName -Location "West Europe"
 
 # Creates 3 tier infrastructure
-New-AzureRmResourceGroupDeployment -Name ToptalDevOpsAcademyDeployment -ResourceGroupName $ResourceGroupName -TemplateFile .\rh3tier.json
+New-AzureRmResourceGroupDeployment -Name ToptalDevOpsAcademyDeployment -ResourceGroupName $ResourceGroupName -TemplateFile .\azuredeploy.json
+
+del C:\Users\Davide\.ssh\known_hosts
+
+ssh -oStrictHostKeyChecking=accept-new dmoraschi@dmoraschi-jump.westeurope.cloudapp.azure.com "curl -sL https://deb.nodesource.com/setup_11.x | sudo -E bash -"
+
+ssh -oStrictHostKeyChecking=accept-new dmoraschi@dmoraschi-jump.westeurope.cloudapp.azure.com "sudo apt-get install -y nodejs"
