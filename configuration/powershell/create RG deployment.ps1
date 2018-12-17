eb0a7c2bf29ef18e9eadb0bde83c35128ed539c0
Davide Moraschi <davide.moraschi@toptal.com>
2018-12-17 17:36:10 +0100

    error with api/status

--- configuration/powershell/create RG deployment.ps1
+++ configuration/powershell/create RG deployment.ps1
@@ -1,7 +1,8 @@
 ﻿$ResourceGroupName="ToptalDevOpsAcademy"
 
 #https://docs.microsoft.com/en-us/azure/virtual-machines/linux/tutorial-automate-vm-deployment
-$customData = Get-Content -Path .\cloud-init.txt 
+$customDataWeb = Get-Content -Raw -Path .\cloud-init-web.txt 
+$customDataApi = Get-Content -Raw -Path .\cloud-init-api.txt 
 
 # Removes Resource Group
 Get-AzureRmResourceGroup -Name $ResourceGroupName | Remove-AzureRmResourceGroup -Verbose -Force
@@ -10,7 +11,7 @@ Get-AzureRmResourceGroup -Name $ResourceGroupName | Remove-AzureRmResourceGroup 
 New-AzureRmResourceGroup -Name $ResourceGroupName -Location "West Europe"
 
 # Creates 3 tier infrastructure
-New-AzureRmResourceGroupDeployment -Name ToptalDevOpsAcademyDeployment -ResourceGroupName $ResourceGroupName -TemplateFile .\azuredeploy.json -adminUsername dmoraschi -customData $customData
+New-AzureRmResourceGroupDeployment -Name ToptalDevOpsAcademyDeployment -ResourceGroupName $ResourceGroupName -TemplateFile .\azuredeploy.json -adminUsername dmoraschi -customDataWeb $customDataWeb -customDataApi $customDataApi
 
 del C:\Users\Davide\.ssh\known_hosts
 
