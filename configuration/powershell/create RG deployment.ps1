12adb3fcb8133176f9445125ce13c2f25d468be8
Davide Moraschi <davide.moraschi@toptal.com>
2018-12-17 15:25:00 +0100

    modified cloud-init.txt

--- configuration/powershell/create RG deployment.ps1
+++ configuration/powershell/create RG deployment.ps1
@@ -1,50 +1,7 @@
 ﻿$ResourceGroupName="ToptalDevOpsAcademy"
 
 #https://docs.microsoft.com/en-us/azure/virtual-machines/linux/tutorial-automate-vm-deployment
-$customData = "#cloud-config
-package_upgrade: true
-packages:
-  - nginx
-  - nodejs
-  - npm
-  - nodejs-legacy
-write_files:
-  - owner: www-data:www-data
-  - path: /etc/nginx/sites-available/default
-    content: |
-      server {
-        listen 80;
-        location / {
-          proxy_pass http://localhost:3000;
-          proxy_http_version 1.1;
-          proxy_set_header Upgrade `$http_upgrade;
-          proxy_set_header Connection keep-alive;
-          proxy_set_header Host $host;
-          proxy_cache_bypass `$http_upgrade;
-        }
-      }
-  - owner: azureuser:azureuser
-  - path: /home/azureuser/myapp/index.js
-    content: |
-      var express = require('express')
-      var app = express()
-      var os = require('os');
-      app.get('/', function (req, res) {
-        res.send('Hello World from host ' + os.hostname() + '!')
-      })
-      app.listen(3000, function () {
-        console.log('Hello world app listening on port 3000!')
-      })
-runcmd:
-  - service nginx restart
-  - cd '/home/azureuser/myapp'
-  - npm init
-  - npm install express -y
-  - nodejs index.js
-  
-# Capture all subprocess output into a logfile
-# Useful for troubleshooting cloud-init issues
-output: {all: '| tee -a /var/log/cloud-init-output.log'}"
+$customData = Get-Content -Path .\cloud-init.txt 
 
 # Removes Resource Group
 Get-AzureRmResourceGroup -Name $ResourceGroupName | Remove-AzureRmResourceGroup -Verbose -Force
